69a5e23853edb5d438e488a3c7e742067b53fa45
Beam <pabeam27@gmail.com>
2024-07-01 20:25:14 +0700

    ีupdated

--- next.config.mjs
+++ next.config.mjs
@@ -25,14 +25,6 @@ const nextConfig = {
             key: 'Cache-Control',
             value: 'no-store, no-cache, must-revalidate, proxy-revalidate',
           },
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: '*',
-          },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: "GET, POST, PUT, DELETE, OPTIONS",
-          },
         ],
       },
     ];
